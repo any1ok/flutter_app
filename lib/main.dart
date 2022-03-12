9b0f749d812f96205666663df8cb9433e9c7ab6e
any1ok <any3ok@naver.com>
2022-03-12 13:27:59 +0900

    padding

--- lib/main.dart
+++ lib/main.dart
@@ -23,50 +23,16 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context){
-    return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Tab'),
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.tag_faces)),
-                Tab(text: '메뉴2'),
-                Tab(icon: Icon(Icons.info),text: '메뉴3'),
-              ],
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              Center(
-                child: Container(
-                  color: Colors.red,
-                  width: 100,
-                  height: 100,
-                )
-              ),
-              Center(
-                  child: Container(
-                    color: Colors.black,
-                    width: 100,
-                    height: 100,
-                  )
-              ),
-              Center(
-                  child: Container(
-                    color: Colors.blue,
-                    width: 100,
-                    height: 100,
-                  )
-              ),
-            ],
-          ),
-          bottomNavigationBar: BottomNavigationBar(items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home',),
-            BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Person',),
-            BottomNavigationBarItem(icon: Icon(Icons.notifications),label: 'Notifiction',),
-          ]),
-        )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('재목'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Container(
+          color: Colors.red,
+        ),
+      ),
     );
   }
 }
